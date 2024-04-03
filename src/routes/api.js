5226d8c9f1b0c9eee08d2423c60c03f435c8a1d7
cuong150104 <nguyenduccuong01152004@gmail.com>
2024-04-03 19:52:50 +0700

    03/04 so khai

---
+++ src/routes/api.js
@@ -0,0 +1,59 @@
+import express from "express";
+import apiController from "../controller/apiController";
+import userController from "../controller/userController";
+import groupController from "../controller/groupController";
+import roleController from "../controller/roleController";
+import { checkUserJWT, checkUserPermission } from "../middleware/JWTAction";
+
+const router = express.Router();
+
+/**
+ *
+ * @param {*} app : express app
+ */
+
+const testMiddleware = (req, res, next) => {
+  console.log("calling a middleware");
+  if (true) {
+    return res.send("reject middleware");
+  }
+  next();
+};
+
+const initApiRoutes = (app) => {
+  // path, handle
+  // rest API
+  // GET - R, POST - C, PUT - U, DELETE - D
+
+  router.all("*", checkUserJWT, checkUserPermission);
+
+  router.post("/register", apiController.handleRegister);
+  router.post("/login", apiController.handleLogin);
+  router.post("/logout", apiController.handleLogout);
+
+  router.get("/account", userController.getUserAccount);
+
+  // user routes
+  router.get("/user/read", userController.readFunc);
+  router.post("/user/create", userController.createFunc);
+  router.put("/user/update", userController.updateFunc);
+  router.delete("/user/delete", userController.deleteFunc);
+
+  // roles routes
+  router.get("/role/read", roleController.readFunc);
+  router.post("/role/create", roleController.createFunc);
+  router.put("/role/update", roleController.updateFunc);
+  router.delete("/role/delete", roleController.deleteFunc);
+  router.get("/role/by-group/:groupId", roleController.getRoleByGroup);
+  router.post("/role/assign-to-group", roleController.assignRoleToGroup);
+
+
+  // group routes
+  router.get("/group/read", groupController.readFunc);
+
+  
+
+  return app.use("/api/v1/", router);
+};
+
+export default initApiRoutes;
---
+++ src/service/userService.js
@@ -0,0 +1,143 @@
+import bcrypt from "bcryptjs";
+import mysql from "mysql2/promise";
+import bluebird from "bluebird";
+import db from "../models/index";
+
+const salt = bcrypt.genSaltSync(10);
+
+const hashUserPassword = (userPassword) => {
+  return bcrypt.hashSync(userPassword, salt);
+};
+
+const createNewUser = async (email, password, username) => {
+  let hashPass = hashUserPassword(password);
+  try {
+    await db.User.create({
+      username: username,
+      email: email,
+      password: hashPass,
+    });
+  } catch (error) {
+    console.log(">> check error: ", error);
+  }
+};
+
+const getUserList = async () => {
+  // test relationships
+  let newUser = await db.User.findOne({
+    where: { id: 1 },
+    attributes: ["id", "username", "email"],
+    include: { model: db.Group, attributes: ["name", "description"] },
+    raw: true,
+    nest: true,
+  });
+
+  let roles = await db.Role.findAll({
+    include: { model: db.Group, where: { id: 1 } },
+    raw: true,
+    nest: true,
+  });
+
+  let user = [];
+  user = await db.User.findAll();
+  return user;
+
+  // const connection = await mysql.createConnection({
+  //   host: "localhost",
+  //   user: "root",
+  //   database: "pbl3_hotel",
+  //   Promise: bluebird,
+  // });
+
+  // try {
+  //   const [rows, fields] = await connection.execute("Select * from user");
+  //   return rows;
+  // } catch (error) {
+  //   console.log(">>> check error: ", error);
+  // }
+};
+
+const deleteUser = async (userId) => {
+  await db.User.destroy({
+    where: {
+      id: userId,
+    },
+  });
+
+  // const connection = await mysql.createConnection({
+  //   host: "localhost",
+  //   user: "root",
+  //   database: "pbl3_hotel",
+  //   Promise: bluebird,
+  // });
+
+  // try {
+  //   const [rows, fields] = await connection.execute(
+  //     "DELETE FROM user WHERE id=?",
+  //     [id]
+  //   );
+  //   return rows;
+  // } catch (error) {
+  //   console.log(">>> check error: ", error);
+  // }
+};
+
+const getUserById = async (id) => {
+  let user = {};
+  user = await db.User.findOne({
+    where: { id: id },
+  });
+  return user.get({ plain: true });
+
+  // const connection = await mysql.createConnection({
+  //   host: "localhost",
+  //   user: "root",
+  //   database: "pbl3_hotel",
+  //   Promise: bluebird,
+  // });
+
+  // try {
+  //   const [rows, fields] = await connection.execute(
+  //     "SELECT * FROM user WHERE id=?",
+  //     [id]
+  //   );
+  //   return rows;
+  // } catch (error) {
+  //   console.log(">>> check error: ", error);
+  // }
+};
+
+const updateUserInfor = async (email, username, id) => {
+  await db.User.update(
+    { email: email, username: username },
+    {
+      where: {
+        id: id,
+      },
+    }
+  );
+  // const connection = await mysql.createConnection({
+  //   host: "localhost",
+  //   user: "root",
+  //   database: "pbl3_hotel",
+  //   Promise: bluebird,
+  // });
+
+  // try {
+  //   const [rows, fields] = await connection.execute(
+  //     "UPDATE user SET email = ?, username = ? WHERE id = ?",
+  //     [email, username, id]
+  //   );
+  //   return rows;
+  // } catch (error) {
+  //   console.log(">>> check error: ", error);
+  // }
+};
+
+module.exports = {
+  createNewUser,
+  getUserList,
+  deleteUser,
+  getUserById,
+  updateUserInfor,
+};
